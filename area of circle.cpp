3cde92edf2575e03041de8ad502f9e5c3cfaa9ca
Tejasri <manchipallitejasri@gmail.com>
2025-11-06 22:26:19 +0530

    Add files via upload

---
+++ area of circle.cpp
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#define PI 3.14
+int main()
+{float rdius, area;
+print f("enter radius:");
+scanf("%f",& radius);
+area = PI*radius*radius;
+print f("\n the area of circle:%.2f",area);
+return 0;
+}
