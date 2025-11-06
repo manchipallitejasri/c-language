83969dd714643879ac55b964b71580715be0ccab
Tejasri <manchipallitejasri@gmail.com>
2025-11-06 20:54:09 +0530

    Add files via upload

---
+++ introduction.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+maim(){
+       int age;
+       float weight;
+       char gender;
+       char name[100];printf("\nenter name");
+       scanf("%s",&name);
+       printf("\nenter age");
+       scanf("%d",&age);
+       printf("\nenter weight");
+       scanf("%f",&weight);
+       printf("\nenter gender");
+       scanf("%c",&gender);
+       printf("\n%s\n%d\n%f\n%c",name,age,weight,gender);
+       }
